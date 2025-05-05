e6e498a7e6623372ae4d0c35f162c1132a0b7f13
Untbu <mail@untbu.de>
2025-05-05 17:32:36 +0200

    configure linter
    Co-authored-by: Julian Horn <mail@julian-horn.net>

--- packages/web/eslint.config.mjs
+++ packages/web/eslint.config.mjs
@@ -1,3 +1,5 @@
+import ts from "typescript-eslint";
+import cspellRecommended from "@cspell/eslint-plugin/recommended";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
@@ -6,11 +8,29 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+    ...ts.configs.recommendedTypeChecked,
+    {
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: __dirname,
+            },
+        },
+    },
+    cspellRecommended,
+    ...compat.extends("next/core-web-vitals", "next/typescript"),
+    {
+        rules: {
+            "linebreak-style": ["error", "unix"],
+            "@typescript-eslint/no-explicit-any": "error",
+            camelcase: "error",
+            "@cspell/spellchecker": "error",
+        },
+    },
 ];
 
 export default eslintConfig;
