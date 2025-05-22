4b2e371ecd48d503597235e9dc433ef5d3765918
Elias Bennour <esieben1310@gmail.com>
2025-05-23 01:05:55 +0200

    link all available pages

--- packages/web/src/app/Footer.component.tsx
+++ packages/web/src/app/Footer.component.tsx
@@ -30,7 +30,7 @@ const navigation = [
     },
     {
         name: "GitHub",
-        href: "#",
+        href: "https://github.com/thm-ops/sose2025",
         icon: (props: React.SVGProps<SVGSVGElement>) => (
             <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
                 <path
