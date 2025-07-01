3adc430080f73fece04e465fd11dfc788d6b027a
Erik Kletzin <erik.kletzin@gmail.com>
2025-07-01 18:49:53 +0200

    fix linter error

--- packages/web/src/app/admin/orders/utils/getStatusStyles.ts
+++ packages/web/src/app/admin/orders/utils/getStatusStyles.ts
@@ -1,14 +1,14 @@
-import { Order } from "./types";
+import { Order, OrderStatus } from "./types";
 
 export const getStatusStyles = (status: Order["status"]): string => {
     switch (status) {
-        case "Pending":
+        case OrderStatus.Pending:
             return "bg-yellow-100 text-yellow-800 ring-yellow-600/20";
-        case "Shipped":
+        case OrderStatus.Shipped:
             return "bg-blue-100 text-blue-800 ring-blue-600/20";
-        case "Delivered":
+        case OrderStatus.Delivered:
             return "bg-green-100 text-green-800 ring-green-600/20";
-        case "Cancelled":
+        case OrderStatus.Cancelled:
             return "bg-red-100 text-red-800 ring-red-600/20";
         default:
             return "bg-gray-100 text-gray-800 ring-gray-600/20";
